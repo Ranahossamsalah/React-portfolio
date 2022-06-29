0081210e84f08aba1eac9905ce179be3ff7b1ca5
Ranahossamsalah <rana.hossam.salah2017@gmail.com>
2022-06-29 17:21:37 +0200

    adding p

--- src/pages/e-commerce/index.jsx
+++ src/pages/e-commerce/index.jsx
@@ -5,6 +5,7 @@ function Ecommerce (){
     const[products,setproduct] =useState()
     return ( <div className="product-wrapper" >
         <h2 className=" text-center m-5 ">My Shop </h2>
+        <p>dkfjgdfkj</p>
         </div>
     )
 }
